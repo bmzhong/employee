f86294f34d754358102921bd1a464b5d471f1628
menglijinghua <508304482@qq.com>
2020-10-16 23:18:53 +0800

    all

--- src/main/java/cn/blabla/controller/ListController.java
+++ src/main/java/cn/blabla/controller/ListController.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("employee")
 public class ListController {
 
-//    @RequestMapping("/")
-//    public String toList(){
-//        return "employee/list";
-//    }
+    @RequestMapping("/toList")
+    public String toList(){
+        return "employee/list";
+    }
 }
